cc7dd861781abb3cf11ef4ec0bd241faf77e71e0
mariamelshamy-20 <marybasemad@gmail.com>
2025-11-30 00:27:24 +0200

    Modify SequentialValidator to consider zeros as empty space
    Updated validation logic to skip zeros in Sudoku checks.

--- SequentialValidator.java
+++ SequentialValidator.java
@@ -3,17 +3,22 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package sudoku;
+
 import java.util.*;
+
 /**
  *
  * @author Mariam Elshamy
  */
 public class SequentialValidator implements SudokuValidator {
-   private final int[][]board;
-   public SequentialValidator(int[][]board){
-       this.board = board ;
-   }
-   @Override
+
+    private final int[][] board;
+
+    public SequentialValidator(int[][] board) {
+        this.board = board;
+    }
+
+    @Override
     public ValidationResult validate(int[][] board) {
 
         List<DuplicateError> allErrors = new ArrayList<>();
@@ -30,18 +35,22 @@ public class SequentialValidator implements SudokuValidator {
             allErrors.addAll(checkBox(b));
         }
 
-        ValidationResult result = new ValidationResult();
+        ValidationResult result = new ValidationResult(board);
         result.valid = allErrors.isEmpty();
         result.errors = allErrors;
 
         return result;
     }
+
     public List<DuplicateError> checkRow(int row) {
 
         Map<Integer, List<int[]>> positions = new HashMap<>();
 
         for (int col = 0; col < 9; col++) {
             int value = board[row][col];
+            if (value == 0) {
+                continue;
+            }
 
             positions.putIfAbsent(value, new ArrayList<>());
             positions.get(value).add(new int[]{row, col});
@@ -49,20 +58,24 @@ public class SequentialValidator implements SudokuValidator {
 
         return buildErrors("ROW", row, positions);
     }
-     public List<DuplicateError> checkColumn(int col) {
+
+    public List<DuplicateError> checkColumn(int col) {
 
         Map<Integer, List<int[]>> positions = new HashMap<>();
 
         for (int row = 0; row < 9; row++) {
             int value = board[row][col];
-
+            if (value == 0) {
+                continue;
+            }
             positions.putIfAbsent(value, new ArrayList<>());
             positions.get(value).add(new int[]{row, col});
         }
 
         return buildErrors("COL", col, positions);
     }
-     public List<DuplicateError> checkBox(int boxIndex) {
+
+    public List<DuplicateError> checkBox(int boxIndex) {
 
         Map<Integer, List<int[]>> positions = new HashMap<>();
 
@@ -75,7 +88,9 @@ public class SequentialValidator implements SudokuValidator {
                 int row = startRow + r;
                 int col = startCol + c;
                 int value = board[row][col];
-
+                if (value == 0) {
+                    continue;
+                }
                 positions.putIfAbsent(value, new ArrayList<>());
                 positions.get(value).add(new int[]{row, col});
             }
@@ -83,8 +98,9 @@ public class SequentialValidator implements SudokuValidator {
 
         return buildErrors("BOX", boxIndex, positions);
     }
-     private List<DuplicateError> buildErrors(String type, int index,
-                                             Map<Integer, List<int[]>> positions) {
+
+    private List<DuplicateError> buildErrors(String type, int index,
+            Map<Integer, List<int[]>> positions) {
 
         List<DuplicateError> errors = new ArrayList<>();
 
@@ -93,16 +109,22 @@ public class SequentialValidator implements SudokuValidator {
             int digit = entry.getKey();
             List<int[]> occ = entry.getValue();
 
-            if (occ.size() > 1) { 
-               DuplicateError error = new DuplicateError(type, index, digit);
-            for (int[] pos : occ) {
-                error.addPosition(pos[0], pos[1]);
+            if (occ.size() > 1) {
+                DuplicateError error = new DuplicateError(type, index, digit);
+                for (int[] pos : occ) {
+                    if (type.equals("ROW")) {
+                        error.addPosition(pos[1]+1);
+                    } else if (type.equals("COL")) {
+                        error.addPosition(pos[0]+1);
+                    } else {
+                        int p=(pos[0]%3)*3 + (pos[1]%3)+1;
+                      error.addPosition(p);
+                    }
+                }
+                errors.add(error);
             }
-            errors.add(error);
         }
-    }
         return errors;
     }
 
- 
 }
