25aeb5dec9efd3c22ddc2b8c9b183946bead7064
miriamatef <miriamatef25@gmail.com>
2025-12-01 00:34:22 +0200

    Update ValidationResult.java

--- ValidationResult.java
+++ ValidationResult.java
@@ -1,3 +1,4 @@
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -5,10 +6,11 @@
 package sudoku;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class ValidationResult {
-
+    
     public boolean valid;
     public List<DuplicateError> errors;
     public int[][] board;   
@@ -16,11 +18,11 @@ public class ValidationResult {
     
     public ValidationResult(int[][] board) {
         this.valid = true;
-        this.errors = new ArrayList<>();
+        this.errors= Collections.synchronizedList(new ArrayList<>());// to make it threadsafe
         this.board = board;
     }
 
-    public void addError(DuplicateError e) {
+    public void  addError(DuplicateError e) {
         this.errors.add(e);
         this.valid = false;
     }
